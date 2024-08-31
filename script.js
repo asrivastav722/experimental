ce55e5924e052c34005dd2dd7df2a2c97dfb123d
Akash Raj <121850110+asrivastav722@users.noreply.github.com>
2024-08-31 10:31:42 +0000

    sidebar added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const sidebar=document.querySelector('.side-buttons');
 
+function showSidebar() {
+    sidebar.style.right="0px";
+    
+}
+function hideSidebar(){
+    sidebar.style.right="-300px"; 
+    
+}
