0383d0c02df39e96bc17746c5b6322862b14204d
Akash Raj <121850110+asrivastav722@users.noreply.github.com>
2024-09-09 16:03:32 +0000

    CSS Added

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
+//Slidebar Area...
 const sidebar=document.querySelector('.sbh');
-
-
 function showSidebar() {
     sidebar.style.right="0px";
-    
-
 }
 function hideSidebar(){
     sidebar.style.right="-100vw"; 
-    
 }
