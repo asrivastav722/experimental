cd3af0f0311d283b14a84458019d2eceafad5fe0
Akash Raj <121850110+asrivastav722@users.noreply.github.com>
2024-09-13 09:58:56 +0000

    experience completed

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ const per=document.querySelector('#personal');
 const pro=document.querySelector('#professional');
 
 function showPers(){
-    document.querySelector('#portprof').style.backgroundColor="rgb(44, 44, 44)";
+    document.querySelector('#portprof').style.backgroundColor="rgb(27, 26, 26)";
     document.querySelector('#portpers').style.backgroundColor="brown";
     per.style.display="flex";
     pro.style.display="none";
 }
 function showProf(){
-    document.querySelector('#portpers').style.backgroundColor="rgb(44, 44, 44)";  
+    document.querySelector('#portpers').style.backgroundColor="rgb(27, 26, 26)";  
     document.querySelector('#portprof').style.backgroundColor="brown";
     pro.style.display="flex";
     per.style.display="none";
